e7b0b6d241626014684239f8957db3ef9f4f738c
Andreas <andreas.giannoutsos@gmail.com>
2021-12-26 03:00:09 +0200

    requiremnets

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements("requirements.txt")
+
+# reqs is a list of requirement
+reqs = [str(ir.req) for ir in install_reqs]
+
 
 setup(name='VolterraConvolutions',
       version='0.1',
@@ -7,4 +14,5 @@ setup(name='VolterraConvolutions',
       author='Andreas Giannoutsos',
       license='MIT',
       packages=['VolterraConvolutions'],
+      install_requires=reqs,
       zip_safe=False)
